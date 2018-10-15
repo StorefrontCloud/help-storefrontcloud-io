9859d18041f10fc99b88c3a22acfeb098807f52c
pkarw <pkarwatka@divante.pl>
2018-10-15 21:52:05 +0200

    Shell script modified to use the right deploy key (via ssh wrapper)

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:StorefrontCloud/help-storefrontcloud-io.git master:gh-pages
+GIT_SSH=../../../sshc git push -f git@github.com:StorefrontCloud/help-storefrontcloud-io.git master:gh-pages
 
 cd -
