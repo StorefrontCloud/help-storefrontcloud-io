10b934b6197c8ae3d9fd6144dc47a11425efa8e4
mkoszutowski <mkoszutowski@divante.pl>
2019-11-26 11:23:59 +0100

    Adding favico

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
     base: '/',
-    head: [['link', { rel: 'icon', href: '/favicon.png' }]],
+    head: [
+      ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png"}],
+      ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicons/favicon-32x32.png"}],
+      ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicons/favicon-16x16.png"}],
+      ['link', { rel: "manifest", href: "/favicons/site.webmanifest"}],
+      ['link', { rel: "mask-icon", href: "/favicons/safari-pinned-tab.svg", color: "#3a0839"}],
+      ['link', { rel: "shortcut icon", href: "/favicons/favicon.ico"}],
+     ],
     themeConfig: {
       repo: 'StorefrontCloud/help-storefrontcloud-io',
       docsDir: 'docs',
